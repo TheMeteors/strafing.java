dec0c8cd0414e21d246c3ca76eb74bd8f9db854b
TheMeteors <meteors12845@gmail.com>
2025-04-15 20:26:49 -0400

    Create strafing2025.java

---
+++ strafing2025.java
@@ -0,0 +1,4 @@
+            double leftFrontPower = power * sin/max - turn;
+            double rightFrontPower = power * sin/max + turn;
+            double leftRearPower = power * cos/max + turn;
+            double rightRearPower = power * cos/max - turn;
